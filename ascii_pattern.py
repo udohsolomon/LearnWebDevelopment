be6fb19f645d000e33c41026af3fa58281799ec6
Solomon <solomonlazio@gmail.com>
2019-01-11 14:40:00 +0000

    update JS

---
+++ ascii_pattern.py
@@ -0,0 +1,19 @@
+word = input('Enter characters: ')
+length = len(word)
+for i in range(0, length):
+    for j in range(length-i -1):
+        print(' ', end = '')
+
+    for j in range(0, i+1):
+        print(word[j], end = '')
+    for k in range(i-1, -1, -1):
+        print(word[k], end = '')
+    print()
+for l in range(1, length):
+    for m in range(l):
+        print(' ', end = '')
+    for n in range(0, length-l-1):
+        print(word[n], end = '')
+    for o in range(length-l-1, -1, -1):
+        print(word[o], end = '')
+    print()
