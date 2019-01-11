8981869acf93cc08f3c941da24388e435db2ba84
Solomon <solomonlazio@gmail.com>
2019-01-11 00:39:41 +0000

    update JS

--- 07_playground.js
+++ 07_playground.js
@@ -49,3 +49,23 @@ var solomon = {
         console.log(this.name) 
     }
 }
+// working with list, objects and functions
+
+var todoList = {
+    todos: ['item 1', 'item 2', 'item 3'],
+    displayTodos: function() {
+        console.log('My todo: ', this.todos)
+    },
+    addTodos: function(todo) {
+        this.todos.push(todo)
+        this.displayTodos()
+    },
+    changeTodos: function(position, newValue) {
+        this.todos[position] = newValue
+        this.displayTodos()
+    },
+    deleteTodos: function(position) {
+        this.todos.splice(position, 1)
+        this.displayTodos()
+    }
+}
